2ec52cda298afe6a1dfedb9563ce9f946c62019a
Anton Nemtsev <thesilentimp@gmail.com>
2015-10-30 23:14:50 +0200

    Update 2015-10-30T21:14:45.565Z

--- service_worker.js
+++ service_worker.js
@@ -4,16 +4,18 @@ var cache_name = 'static_assets';
 // Вызывается при первом вызове воркера страницей
 this.addEventListener('install', function(event) {
 
-    caches.open(cache_name).then(function(cache) {
-        return cache.addAll([
-            'https://silentimp.github.io/service-worker-demo/', 'https://silentimp.github.io/service-worker-demo/index.html', 'https://silentimp.github.io/service-worker-demo/css/styles.css', 'https://silentimp.github.io/service-worker-demo/images/photo.jpg'
-        ]);
-        console.log('Воркер инсталирован, данные кэшированы');
+    event.waitUntil(
+        caches.open(cache_name).then(function(cache) {
+            return cache.addAll([
+                'https://silentimp.github.io/service-worker-demo/', 'https://silentimp.github.io/service-worker-demo/index.html', 'https://silentimp.github.io/service-worker-demo/css/styles.css', 'https://silentimp.github.io/service-worker-demo/images/photo.jpg'
+            ]);
+            console.log('Воркер инсталирован, данные кэшированы');
 
-    }).catch(function(err) {
-        console.log('Не удалось кешировать данные', err);
+        }).catch(function(err) {
+            console.log('Не удалось кешировать данные', err);
 
-    });
+        });
+    );
 
 });
 
